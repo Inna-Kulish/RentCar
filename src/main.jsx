6ffbc013e4508a9a48c015ca1c69afcc0450036e
Inna-Kulish <norelajest@gmail.com>
2023-11-20 23:48:27 +0200

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
-import { Provider } from "react-redux";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
+   <React.StrictMode>
     <BrowserRouter basename="/RentCar">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
@@ -16,4 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </PersistGate>
       </Provider>
     </BrowserRouter>
+    </React.StrictMode>
 );
