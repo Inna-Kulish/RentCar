62d798d06e304913492ca060e9f08f31b824cb95
Inna-Kulish <Inna-Kulish@users.noreply.github.com>
2023-11-21 18:31:26 +0000

    deploy: 9e10803aaf1519d375bfed0e4d5e19c5610024a2

---
+++ assets/FavoritePage-ade6d939.js
@@ -0,0 +1,8 @@
+import{s,f as o,j as t}from"./index-775a05ca.js";import{d as a,C as r,c as i}from"./CatalogPage.styled-386d9a9c.js";const n=s.h1`
+text-align: center;
+margin-bottom: 46px;
+color: #121417;
+font-family: 'ManropeMedium';
+font-size: 44px;
+line-height: 20px;
+`;function x(){const e=o(a);return t.jsxs(r,{children:[t.jsx(n,{children:"Favorites"}),t.jsx(i,{cars:e})]})}export{x as default};
---
+++ assets/HomePage-880d33db.js
@@ -0,0 +1,29 @@
+import{s as n,j as e}from"./index-775a05ca.js";const t="/RentCar/assets/map-d32b8acc.jpg",a=n.section`
+width: 100vw;
+height: 170px;
+padding-top: 90px;
+
+background-image: linear-gradient(
+      rgba(46, 47, 66, 0.7),
+      rgba(46, 47, 66, 0.7)
+    ),
+    url(${t});
+     background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`,i=n.h1`
+text-align: center;
+margin-bottom: 46px;
+font-family: 'ManropeMedium';
+font-size: 44px;
+line-height: 20px;
+color: white;
+`,o=n.ul`
+display: flex;
+  flex-wrap: wrap;
+  column-gap: 24px;
+  row-gap: 48px;
+
+`,r=n.li`
+
+`;function p(){return e.jsx(e.Fragment,{children:e.jsxs("section",{children:[e.jsx(a,{children:e.jsx(i,{children:"Car rental in Ukraine"})}),e.jsxs(o,{children:[e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{})]})]})})}export{p as default};
