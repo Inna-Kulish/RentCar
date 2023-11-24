9dea45d9cd5370612d1d4ec171fe624589a6c668
Inna-Kulish <Inna-Kulish@users.noreply.github.com>
2023-11-24 21:14:50 +0000

    deploy: 431328bc0b4439d431b2a6ce6c77e79bee4ea262

---
+++ assets/HomePage-e6f150ac.js
@@ -0,0 +1,54 @@
+import{s as a,r as t,j as e}from"./index-1ac1d157.js";const o="/RentCar/assets/map-d32b8acc.jpg",h=a.section`
+  width: 100vw;
+  height: 170px;
+  padding-top: 90px;
+
+  background-image: linear-gradient(
+      rgba(46, 47, 66, 0.7),
+      rgba(46, 47, 66, 0.7)
+    ),
+    url(${o});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`,d=a.section`
+padding-top: 15px;
+`,l=a.section`
+  padding-left: 15px;
+  padding-right: 15px;
+`,p=a.h1`
+  text-align: center;
+  margin-bottom: 46px;
+  font-family: "ManropeMedium";
+  font-size: 44px;
+  line-height: 20px;
+  color: white;
+`,x=a.ul`
+  display: flex;
+  column-gap: 24px;
+  row-gap: 48px;
+`,s=a.li`
+  padding: 30px;
+  flex-basis: calc((100% - (6px * 3)) / 4);
+  border: 1px solid #c3c3c3;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+`,r=a.svg`
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  width: 50px;
+  height: 50px;
+  fill: #283c86;
+`,i=a.h2`
+  margin-top: 24px;
+  color: #283c86;
+  font-family: "ManropeSemiBolt";
+  font-size: 17px;
+  text-align: center;
+`,c=a.p`
+  margin-top: 14px;
+  font-size: 14px;
+  line-height: calc(24 / 14);
+  letter-spacing: 0.02em;
+  text-align: center;
+`,g=n=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:512,height:512,"data-name":"Layer 1",viewBox:"0 0 24 24",...n},t.createElement("path",{d:"M24 13a3 3 0 0 0-3-3h-.478L15.84 3.285A3 3 0 0 0 13.379 2h-7a3.016 3.016 0 0 0-2.804 1.937l-2.6 6.848A2.994 2.994 0 0 0 0 13v5h2v.5a3.5 3.5 0 0 0 7 0V18h6v.5a3.5 3.5 0 0 0 7 0V18h2Zm-9.8-8.572L18.084 10H11V4h2.379a1 1 0 0 1 .821.428Zm-8.753.217A1 1 0 0 1 6.381 4H9v6H3.416ZM7 18.5a1.5 1.5 0 0 1-3 0V18h3Zm13 0a1.5 1.5 0 0 1-3 0V18h3Zm2-2.5H2v-3a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1Z"})),m=g,v=n=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:512,height:512,viewBox:"0 0 24 24",...n},t.createElement("path",{d:"M19 4h-1.1A5.009 5.009 0 0 0 13 0h-2a5.009 5.009 0 0 0-4.9 4H5a5.006 5.006 0 0 0-5 5v10a5.006 5.006 0 0 0 5 5h14a5.006 5.006 0 0 0 5-5V9a5.006 5.006 0 0 0-5-5Zm-8-2h2a3 3 0 0 1 2.816 2H8.184A3 3 0 0 1 11 2ZM5 6h14a3 3 0 0 1 3 3v3H2V9a3 3 0 0 1 3-3Zm14 16H5a3 3 0 0 1-3-3v-5h9v1a1 1 0 0 0 2 0v-1h9v5a3 3 0 0 1-3 3Z"})),u=v,j=n=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg","data-name":"Layer 1",viewBox:"0 0 24 24",...n},t.createElement("path",{d:"M22.802.703A1.482 1.482 0 0 0 21.537 0h-4.025a3.479 3.479 0 0 0-3.139 1.952l-2.001 4.057a9.144 9.144 0 0 0-.742 0L9.629 1.952A3.478 3.478 0 0 0 6.49 0H2.463a1.48 1.48 0 0 0-1.265.703c-.273.44-.3.98-.071 1.445l3.674 7.455a8.954 8.954 0 0 0-1.802 5.396c0 4.962 4.038 9 9 9s9-4.038 9-9a8.95 8.95 0 0 0-1.802-5.396l3.674-7.455A1.48 1.48 0 0 0 22.8.703ZM2.024 1.706a.485.485 0 0 1 .023-.475A.483.483 0 0 1 2.463 1h4.025c.958 0 1.818.534 2.242 1.394l1.836 3.721a8.99 8.99 0 0 0-5.059 2.659L2.024 1.706ZM12 23c-4.411 0-8-3.589-8-8 0-4.176 3.744-8 8-8s8 3.825 8 8c0 4.411-3.589 8-8 8Zm9.976-21.294-3.484 7.068a8.99 8.99 0 0 0-5.059-2.659l1.836-3.721A2.485 2.485 0 0 1 17.511 1h4.025c.246 0 .373.162.416.231a.482.482 0 0 1 .023.475ZM15.894 13h-2.025l-.536-1.961A1.404 1.404 0 0 0 11.991 10a1.396 1.396 0 0 0-1.322 1.034L10.131 13H8.106c-.792 0-1.283.635-1.283 1.224 0 .545.331 1.068.843 1.352l1.331.829-.604 1.646a1.44 1.44 0 0 0 .489 1.657 1.447 1.447 0 0 0 1.758-.011l1.361-1.052 1.361 1.052a1.454 1.454 0 0 0 1.758.011c.514-.387.71-1.053.489-1.657l-.604-1.646 1.309-.816c.533-.296.864-.819.864-1.365 0-.589-.491-1.224-1.283-1.224Zm-.088 1.728-1.676 1.044a.5.5 0 0 0-.205.597l.744 2.028a.452.452 0 0 1-.152.513.453.453 0 0 1-.545-.003l-1.667-1.289a.497.497 0 0 0-.611 0l-1.667 1.289a.45.45 0 0 1-.697-.51l.744-2.028a.5.5 0 0 0-.205-.597l-1.697-1.057c-.174-.097-.35-.286-.35-.491 0-.06.067-.224.283-.224h2.407a.499.499 0 0 0 .482-.368l.64-2.34A.39.39 0 0 1 11.99 11a.397.397 0 0 1 .376.297l.638 2.334a.5.5 0 0 0 .482.368h2.407c.216 0 .283.164.283.224 0 .205-.176.394-.371.503Z"})),w=j,f=n=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg","data-name":"Layer 1",viewBox:"0 0 24 24",...n},t.createElement("path",{d:"M3.688 24h-.095a3.664 3.664 0 0 1-2.649-1.224c-1.269-1.409-1.157-3.784.244-5.185l5.868-5.867c.253-.254.344-.631.241-1.009a8.482 8.482 0 0 1-.102-4.036C7.903 3.364 10.626.735 13.972.137A8.482 8.482 0 0 1 16.974.13a1.914 1.914 0 0 1 1.52 1.325c.251.799-.003 1.681-.682 2.359l-2.247 2.217c-.658.658-.758 1.69-.222 2.345.308.378.742.598 1.222.622a1.7 1.7 0 0 0 1.271-.489l2.58-2.55a2.044 2.044 0 0 1 2.07-.501 2.01 2.01 0 0 1 1.385 1.567 8.559 8.559 0 0 1-.007 3.003c-.6 3.347-3.229 6.07-6.544 6.777a8.468 8.468 0 0 1-4.036-.103c-.377-.104-.754-.012-1.008.241L6.3 22.918a3.708 3.708 0 0 1-2.612 1.081ZM15.61 1.993c-.422 0-.854.035-1.286.112-2.554.457-4.634 2.463-5.174 4.991a6.49 6.49 0 0 0 .076 3.093c.29 1.062 0 2.191-.756 2.948l-5.868 5.867c-.65.65-.732 1.81-.171 2.433.315.35.747.55 1.215.562.461.019.909-.163 1.241-.494l5.975-5.975c.755-.755 1.885-1.047 2.948-.757 1.004.274 2.045.3 3.093.076 2.528-.539 4.534-2.618 4.992-5.174a6.54 6.54 0 0 0 .006-2.301v-.007l-2.655 2.559a3.622 3.622 0 0 1-2.781 1.068 3.693 3.693 0 0 1-2.669-1.353c-1.179-1.439-1.021-3.649.361-5.03l2.247-2.217c.179-.18.191-.314.184-.341a7.964 7.964 0 0 0-.976-.062Z"})),b=f;function Z(){return e.jsxs(e.Fragment,{children:[e.jsx(h,{children:e.jsx(l,{children:e.jsx(p,{children:"Car rental in Ukraine"})})}),e.jsx(d,{children:e.jsx(l,{children:e.jsxs(x,{children:[e.jsxs(s,{children:[e.jsx("div",{children:e.jsx(r,{as:w,stroke:"#283c86"})}),e.jsx(i,{children:"Experienced Car Rental in Ukraine"}),e.jsx(c,{children:"Embark on a journey of freedom and unforgettable moments with RentCar. We are your reliable travel companion, offering a diverse range of vehicles at affordable prices."})]}),e.jsxs(s,{children:[e.jsx("div",{children:e.jsx(r,{as:m})}),e.jsx(i,{children:"Cutting-edge Fleet"}),e.jsx(c,{children:"Your comfort is our priority. Choose from Kia and Nissan to Lincoln - always ready at your disposal."})]}),e.jsxs(s,{children:[e.jsx("div",{children:e.jsx(r,{as:u})}),e.jsx(i,{children:"Business Trips and Leisure Getaways"}),e.jsx(c,{children:"We offer flexible rental terms, tailored for both business and pleasure. Our commitment is to make your travel experience seamless and enjoyable."})]}),e.jsxs(s,{children:[e.jsx("div",{children:e.jsx(r,{as:b})}),e.jsx(i,{children:"24/7 Support"}),e.jsx(c,{children:"Our dedicated support team is available around the clock to assist you. Enjoy peace of mind knowing that help is just a call away."})]})]})})})]})}export{Z as default};
